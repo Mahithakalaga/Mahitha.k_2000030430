79289fe23cc5ff083743270ce334ac1b64e97c3b
Mahithakalaga <2000030430@kluniversity.in>
2023-01-07 09:44:34 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Mahitha.k_2000030430'`, () => {
+  it(`should have as title 'Space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Mahitha.k_2000030430');
+    expect(app.title).toEqual('Space');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Mahitha.k_2000030430 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Space app is running!');
   });
 });
+
+
